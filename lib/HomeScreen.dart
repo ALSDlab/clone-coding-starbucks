ba95f25a85947966ab18fe4128db12b71b99937e
ssd72 <sungdae.yi@gmail.com>
2023-12-22 14:22:08 +0100

    12월 22일 클론 코딩 스타벅스 - 추가수정(횡스크롤뷰를 포함한 스크롤뷰 요소들을 컬렉션하여 랜덤한 위치에 놓이게 변경)

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -14,196 +14,164 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final cardInfos = ImageFileRepository().getCards();
+  List<Widget> sliverList = [];
+
+  @override                                       // 초기화로 종방향 리스트뷰와 횡스크롤뷰를 컬렉선하고 처음 위치에
+  void initState() {                              // SliverAppBar 를 넣은다음 랜덤하게 위치 조정.
+    super.initState();                            // 나중에 횡스크롤뷰를 원하는 위치에 넣을 수 있음
+    for (int i = 0; i < cardInfos.length; i++) {
+      sliverList.add(
+        SliverToBoxAdapter(
+          child: StarbucksCard(
+            card: cardInfos[i],
+          ),
+        ),
+      );
+    }
+    sliverList.add(const WhatIsNewCards());
+    sliverList.shuffle();
+    sliverList.insert(0, SliverAppBar(
+      surfaceTintColor: Colors.white,
+      backgroundColor: Colors.white,
+      pinned: true,
+      expandedHeight: 300.0,
+      flexibleSpace: FlexibleSpaceBar(
+        title: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      elevation: 0,
+                      backgroundColor: Colors.white),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.email_outlined,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        '''What's New''',
+                        style:
+                        TextStyle(color: Colors.black, fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      elevation: 0,
+                      backgroundColor: Colors.white),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.confirmation_number_outlined,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        '''Coupon''',
+                        style:
+                        TextStyle(color: Colors.black, fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  width: 50,
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.notifications_none))
+              ],
+            ),
+          ),
+        ),
+        background: Column(
+          children: [
+            Image.asset('assets/images/starbucks_01.png'),
+            const Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '5 ★ until Green Level',
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                                top: 10.0, right: 10.0, bottom: 10.0),
+                            child: Opacity(
+                              opacity: 0.3,
+                              child: SimpleAnimationProgressBar(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(10)),
+                                height: 6,
+                                width: 270,
+                                backgroundColor: Colors.grey,
+                                foregrondColor: Colors.green,
+                                ratio: 0.0,
+                                direction: Axis.horizontal,
+                                curve: Curves.fastLinearToSlowEaseIn,
+                                duration: Duration(seconds: 3),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Text(
+                        '0',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      Text(
+                        '/5★',
+                        style:
+                        TextStyle(fontSize: 20, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            surfaceTintColor: Colors.white,
-            backgroundColor: Colors.white,
-            pinned: true,
-            expandedHeight: 300.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8, right: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: Size.zero,
-                            padding: const EdgeInsets.only(left: 16, right: 16),
-                            elevation: 0,
-                            backgroundColor: Colors.white),
-                        child: const Row(
-                          children: [
-                            Icon(
-                              Icons.email_outlined,
-                              color: Colors.black,
-                            ),
-                            SizedBox(width: 5),
-                            Text(
-                              '''What's New''',
-                              style: TextStyle(color: Colors.black, fontSize: 15),
-                            ),
-                          ],
-                        ),
-                        onPressed: () {},
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: Size.zero,
-                            padding: const EdgeInsets.only(left: 16, right: 16),
-                            elevation: 0,
-                            backgroundColor: Colors.white),
-                        child: const Row(
-                          children: [
-                            Icon(
-                              Icons.confirmation_number_outlined,
-                              color: Colors.black,
-                            ),
-                            SizedBox(width: 5),
-                            Text(
-                              '''Coupon''',
-                              style: TextStyle(color: Colors.black, fontSize: 15),
-                            ),
-                          ],
-                        ),
-                        onPressed: () {},
-                      ),
-                      const SizedBox(
-                        width: 50,
-                      ),
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.notifications_none))
-                    ],
-                  ),
-                ),
-              ),
-              background: Column(
-                children: [
-                  Image.asset('assets/images/starbucks_01.png'),
-                  const Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 16.0, right: 16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '5 ★ until Green Level',
-                                  style:
-                                      TextStyle(fontSize: 15, color: Colors.grey),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      top: 10.0, right: 10.0, bottom: 10.0),
-                                  child: Opacity(
-                                    opacity: 0.3,
-                                    child: SimpleAnimationProgressBar(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(10)),
-                                      height: 6,
-                                      width: 270,
-                                      backgroundColor: Colors.grey,
-                                      foregrondColor: Colors.green,
-                                      ratio: 0.0,
-                                      direction: Axis.horizontal,
-                                      curve: Curves.fastLinearToSlowEaseIn,
-                                      duration: Duration(seconds: 3),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                            Text(
-                              '0',
-                              style: TextStyle(fontSize: 40),
-                            ),
-                            Text(
-                              '/5★',
-                              style: TextStyle(fontSize: 20, color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[0],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[1],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[2],
-            ),
-          ),
-          const WhatIsNewCards(),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[3],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[4],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[5],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[6],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[7],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[8],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[9],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: StarbucksCard(
-              card: cardInfos[10],
-            ),
-          ),
-        ],
+        slivers: sliverList,
       ),
       floatingActionButton: FloatingActionButton(
         shape: const CircleBorder(),
-        onPressed: (){},
+        onPressed: () {},
         tooltip: 'Increment',
         backgroundColor: Colors.green,
-        child: const Icon(Icons.moped_rounded,color: Colors.white,size: 30,),
+        child: const Icon(
+          Icons.moped_rounded,
+          color: Colors.white,
+          size: 30,
+        ),
       ),
     );
   }
--- lib/repository/cardModelRepository.dart
+++ lib/repository/cardModelRepository.dart
@@ -18,6 +18,66 @@ class ImageFileRepository {
       description: '',
     ),
     CardDetail(
+      id: 4,
+      filePath: 'assets/images/04_01_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 5,
+      filePath: 'assets/images/04_02_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 6,
+      filePath: 'assets/images/04_03_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 7,
+      filePath: 'assets/images/04_04_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 8,
+      filePath: 'assets/images/04_05_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 9,
+      filePath: 'assets/images/04_06_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 10,
+      filePath: 'assets/images/04_07_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 11,
+      filePath: 'assets/images/04_08_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 12,
+      filePath: 'assets/images/04_09_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 13,
+      filePath: 'assets/images/04_10_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 14,
+      filePath: 'assets/images/04_11_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
+      id: 15,
+      filePath: 'assets/images/04_12_cardnews.png',
+      description: '',
+    ),
+    CardDetail(
       id: 16,
       filePath: 'assets/images/05_01_card.png',
       description: '',
